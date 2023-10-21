38e5f4f3cf93b91f4d5b349c82ea7573310ccf34
itsxrp <itsxrproms@gmail.com>
2023-10-21 09:36:10 +0200

    sm8350-common: Welcome ProjectBlaze
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- common.mk
+++ common.mk
@@ -303,7 +303,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-blaze
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
